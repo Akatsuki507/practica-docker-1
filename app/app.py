70b73db786e2d3e41624bc3451ec00c6ee6d7ca3
Alfonso Alvarado <Alfonso.Alvarado.v31@gmail.com>
2023-06-10 18:37:00 -0500

    fix app container to allow multiple scripts

---
+++ app/app.py
@@ -0,0 +1,12 @@
+# funcion que sume dos numeros
+def suma(a, b):
+    return a + b
+
+# Path: app\app.py
+# funcion que reste dos numeros
+def resta(a, b):
+    return a - b
+
+print("Hola, me has atrapado, en medio suavemente")
+print(suma(1, 2))
+print(resta(1, 2))
